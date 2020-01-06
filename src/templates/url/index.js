8ab4979ed519cd16e79b1fc6f9081d98c31f1c61
Prakhar Agarwal <prakhargvp@gmail.com>
2020-01-06 20:43:29 +0530

    Adding Searchbar with tabs

--- src/templates/url/index.js
+++ src/templates/url/index.js
@@ -32,6 +32,10 @@ import ThumbDown from '@material-ui/icons/ThumbDown';
 import ThumbUp from '@material-ui/icons/ThumbUp';
 import Box from '@material-ui/core/Box'; 
 import * as _ from 'lodash';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Brightness4Icon from '@material-ui/icons/Brightness4'; 
+import Brightness7Icon from '@material-ui/icons/Brightness7'; 
+import AppsIcon from '@material-ui/icons/Apps'; 
 import SideBarNavLinks from './sidebar';
 import SearchBar from './SearchBar';
 import { topics } from './url';
@@ -129,7 +133,7 @@ TabPanel.propTypes = {
 export default function PersistentDrawerLeft() {
   const classes = useStyles();
   const theme = useTheme();
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -142,6 +146,8 @@ export default function PersistentDrawerLeft() {
     setValue(newValue);
  };
 
+const [contentTab, setContentTab] = React.useState(1);
+
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -152,19 +158,43 @@ export default function PersistentDrawerLeft() {
           { [classes.appBarShift]: open }
         )}
       >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            
-          </Typography>
+        <Toolbar style={{ justifyContent: 'space-between' }}>
+         <div>
+	          <IconButton
+	            color="inherit"
+	            aria-label="open drawer"
+	            onClick={handleDrawerOpen}
+	            edge="start"
+	            className={clsx(classes.menuButton, open && classes.hide)}
+	          >
+	            <MenuIcon />
+	          </IconButton>
+          </div>
+     	  <div>
+
+			
+     	  	<IconButton
+	            aria-label="Toggle light/dark theme"
+	            color="inherit"
+	          >
+	            {1 ==1 ? <Brightness4Icon /> : <Brightness7Icon />}
+	        </IconButton>
+	        <IconButton
+	            aria-label="Apps"
+	            color="inherit"
+	          >
+				<AppsIcon />
+	        </IconButton>
+	        <IconButton
+	            aria-label="account of current user"
+	            aria-controls="menu-appbar"
+	            aria-haspopup="true"
+	            color="inherit"
+	          >
+	            <AccountCircle />
+	        </IconButton>
+
+          </div>
         </Toolbar>
       </AppBar>
       <Drawer
@@ -194,26 +224,31 @@ export default function PersistentDrawerLeft() {
       >
       	<div className={classes.drawerHeader} />
       	<SearchBar />
-      	<Tabs
-          value={value}
-          onChange={handleChange}
-          variant="scrollable"
-          scrollButtons="on"
-          indicatorColor="primary"
-          textColor="primary"
-          aria-label="scrollable force tabs example"
-        >
-          <Tab label="Item One" icon={<PhoneIcon />} {...a11yProps(0)} />
-          <Tab label="Item Two" icon={<FavoriteIcon />} {...a11yProps(1)} />
-          <Tab label="Item Three" icon={<PersonPinIcon />} {...a11yProps(2)} />
-          <Tab label="Item Four" icon={<HelpIcon />} {...a11yProps(3)} />
-          <Tab label="Item Five" icon={<ShoppingBasket />} {...a11yProps(4)} />
-          <Tab label="Item Six" icon={<ThumbDown />} {...a11yProps(5)} />
-          <Tab label="Item Seven" icon={<ThumbUp />} {...a11yProps(6)} />
-        </Tabs>
- 		<TabPanel value={value} index={0}>
-        	Item One
-      	</TabPanel>
+
+      	{contentTab ? (
+      	<Fragment>
+	      	<Tabs
+	          value={value}
+	          onChange={handleChange}
+	          variant="scrollable"
+	          scrollButtons="on"
+	          indicatorColor="primary"
+	          textColor="primary"
+	          aria-label="scrollable force tabs example"
+	        >
+	          <Tab label="Item One" icon={<PhoneIcon />} {...a11yProps(0)} />
+	          <Tab label="Item Two" icon={<FavoriteIcon />} {...a11yProps(1)} />
+	          <Tab label="Item Three" icon={<PersonPinIcon />} {...a11yProps(2)} />
+	          <Tab label="Item Four" icon={<HelpIcon />} {...a11yProps(3)} />
+	          <Tab label="Item Five" icon={<ShoppingBasket />} {...a11yProps(4)} />
+	          <Tab label="Item Six" icon={<ThumbDown />} {...a11yProps(5)} />
+	          <Tab label="Item Seven" icon={<ThumbUp />} {...a11yProps(6)} />
+	        </Tabs>
+	 		<TabPanel value={value} index={0}>
+	        	Item One
+	      	</TabPanel>
+      	</Fragment>
+      	): ''}
        
       </main>
     </div>
